7aad8a4501100213db94c1fa3737027e0beebfe4
zhanshen886 <1875370642@qq.com>
2025-12-24 20:13:22 +0800

    接口域名替换

--- src/app/api/service/axios.ts
+++ src/app/api/service/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // 创建axios实例
 const instance = axios.create({
-  baseURL: 'http://localhost:3001', // API的基础路径
+  baseURL: '/', // API的基础路径
   timeout: 10000, // 请求超时时间
 });
 
